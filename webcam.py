3cb9859423a94a911fe70437cb54498cce2394a3
Jules LaPrairie <jlaprairie@gmail.com>
2018-05-28 00:58:49 -0600

    Added a delete url

--- webcam.py
+++ webcam.py
@@ -1,11 +1,14 @@
 from flask import Flask, send_from_directory
 from picamera import PiCamera
+import os
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
 	return "I'm a camera!"
+
 
 @app.route("/capture/<filename>")
 def capture(filename):
@@ -17,11 +20,20 @@ def capture(filename):
 		camera.close()
 	return "Captured: " + fullname
 
+
 @app.route("/download/<filename>")
 def download(filename):
 	return send_from_directory('.', filename + '.jpg')
 
+
+@app.route("/delete/<filename>")
+def delete(filename):
+    try:
+        os.remove(filename + '.jpg')
+        return "Deleted: " + filename
+    except OSError:
+        return filename + " not found."
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True)
-
-
