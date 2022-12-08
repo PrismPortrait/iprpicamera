7e6111b5a699d961205d4ea62b3982f9ab955b84
Daniel Wehr <daniel.wehr@energytoolbase.com>
2022-12-08 15:00:34 -0700

    Add UDP trigger for image capture, API method should still work too

--- webcam.py
+++ webcam.py
@@ -1,6 +1,9 @@
+import os
+import socket
+from threading import RLock, Thread
+
 from flask import Flask, send_from_directory
 from picamera import PiCamera
-import os
 
 app = Flask(__name__)
 
@@ -12,11 +15,9 @@ def home():
 
 @app.route("/capture/<filename>")
 def capture(filename):
-    try:
-        fullname = filename + '.jpg'
+    fullname = filename + '.jpg'
+    with camera_lock:
         camera.capture(fullname)
-    finally:
-        camera.close()
     return "Captured: " + fullname
 
 
@@ -37,5 +38,31 @@ def delete(filename):
         return filename + " not found."
 
 
+def udp_receiver(camera):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 40000))
+
+    while True:
+        # Capture image to received filename.
+        # TODO: Investigate faster options than just calling .capture
+        sock.recv(1024)
+        with camera_lock:
+            camera.capture(camera_num + ".jpg")
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    camera = None
+    try:
+        # Pull camera number from hostname.
+        camera_num = socket.gethostname()[-1]
+        # Init camera where both API and udp can use.
+        camera = PiCamera()
+        camera_lock = RLock()
+        # Start receiver as daemon, it will close if main thread closes.
+        udp_thread = Thread(target=udp_receiver, daemon=True)
+        udp_thread.start()
+        app.run(host='0.0.0.0', port=80, debug=True)
+    finally:
+        if camera is not None:
+            camera.close()
